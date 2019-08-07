2c54d8f0f142d4b9354c670407083c709dabfd45
mzahng160 <mzhang160@163.com>
2019-08-07 17:48:03 +0800

    add s10

---
+++ s10/Callbacks.h
@@ -0,0 +1,24 @@
+#ifndef MUDUO_NET_CALLBACKS_H
+#define MUDUO_NET_CALLBACKS_H
+
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "datetime/Timestamp.h"
+namespace muduo
+{
+	class Buffer;
+	class TcpConnection;
+
+	typedef boost::shared_ptr<TcpConnection> TcpConnectionPtr;
+
+	typedef boost::function<void()> TimerCallback;
+	typedef boost::function<void (const TcpConnectionPtr&)> ConnectionCallback;
+	typedef boost::function<void (const TcpConnectionPtr&,
+									Buffer* buf, 
+									Timestamp)> MessageCallback;
+	typedef boost::function<void (const TcpConnectionPtr&)> WriteCompleteCallback;
+	typedef boost::function<void (const TcpConnectionPtr&)> CloseCallback;
+}
+
+#endif //MUDUO_NET_CALLBACKS_H
