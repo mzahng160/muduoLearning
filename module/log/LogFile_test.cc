257ea60c15ecbc4ea6bc901879daf7745eabc7d4
mzahng160 <mzhang160@163.com>
2020-02-01 15:33:37 +0800

    change Makefile-bak to Makefile-Logging_test

--- module/log/LogFile_test.cc
+++ module/log/LogFile_test.cc
@@ -1,21 +1,34 @@
 #include "LogFile.h"
 #include "Logging.h"
 
+#include <unistd.h>
+
 std::unique_ptr<LogModule::LogFile> g_logFile;
 
-void outputFunc()
+void outputFunc(const char* msg, int len)
 {
-
+	g_logFile->append(msg, len);
 }
 
 void flushFunc()
 {
-
+	printf("[%s flushFunc] flush!", __FILE__);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+	using namespace LogModule;
 	char name[256] = {0};
 	strncpy(name, argv[0], sizeof name -1);
 	g_logFile.reset(new LogModule::LogFile(::basename(name), 200*1000));
+	LogModule::Logger::setOutput(outputFunc);
+	LogModule::Logger::setFlush(flushFunc);
+
+	std::string line = "1234567890 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+	for (int i = 0; i < 10000; ++i)
+	{
+		LOG_INFO << line << i;
+		usleep(1000);
+	}
 }
